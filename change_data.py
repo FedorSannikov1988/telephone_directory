f7aae0b3db47d85b9696e0a75ffccb1080c7fb78
Анастасия Тибейкина <Nastya@kompas.lan>
2022-12-15 14:11:59 +0600

    функция изменения данных

---
+++ change_data.py
@@ -0,0 +1,38 @@
+import add_data
+
+def change_data_in_telephone_directory(data: list) -> list:
+
+    print("\n")
+    print("\t ПРОЦЕДУРА ИЗМЕНЕНИЯ ДАННЫХ:")
+    print("\t---------------------------")
+    
+    while True:
+        text = "\tВведите номер строки \nкоторую необходимо изменить после чего нажмите 'Enter'\n"
+        
+        human_answer_string = input(f"{text}")
+
+        if not human_answer_string.isdigit():
+            print("Повторите ввод")
+            continue
+        try:
+            human_answer_namber = int(human_answer_string)
+            break
+        except ValueError:
+            print("Повторите ввод")
+            continue
+
+    change_element = []
+
+    need_change_data = add_data.input_data_in_phone_directory()
+
+
+        # элемент, который надо изменить и его индекс
+    change_element.append(data[human_answer_namber-1])
+    index_changing_element = human_answer_namber - 1
+
+    for i in range(0, len(data)):
+        if i == index_changing_element:
+            for i in range(0, len(need_change_data)):
+                if need_change_data[i] != '' : change_element[0][i] = need_change_data[i]
+                else: i +=1
+    return data
