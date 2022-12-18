f32f13aa0962791f285043dc500062723e3813fb
Nastya <tibeikina@inbox.ru>
2022-12-18 10:55:51 +0600

    так работает

--- search_data.py
+++ search_data.py
@@ -1,7 +1,5 @@
 import print_data
 
-
-
 def search_data_from_list(data: list):
 
     print("\n")
@@ -19,5 +17,18 @@ def search_data_from_list(data: list):
  
     if data_from_serch == []: print('Совпадений не найдено!')
     else: print(print_data.print_all_list_in_terminal(data_from_serch))
-
+    print(data_from_serch)
     return data_from_serch
+
+# def search_data_from_list(all_data: list, need_find_data: list) -> list:
+
+#     data_from_serch = []
+
+#     for count in range(0, len(all_data), 1):
+#         if [all_data[count][0]].count(need_find_data[0]) or\
+#                 [all_data[count][1]].count(need_find_data[1]) or\
+#                 [all_data[count][2]].count(need_find_data[2]) or\
+#                     [all_data[count][3]].count(need_find_data[3]):
+#                     data_from_serch.append(all_data[count])
+#     print(data_from_serch)
+#     return data_from_serch
